b901c6485e278fa7cabf9898bfdf35f4b053e354
Aysel Cavdar <a.misrikhanova13@gmail.com>
2021-11-30 16:15:50 +0300

    change app.js

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
+
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
@@ -32,7 +32,7 @@ function App() {
   }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   );
 }
