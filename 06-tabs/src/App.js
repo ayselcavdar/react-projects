1689dd7a152aaf8836f9cdcac456a32cf649b8ff
Aysel Cavdar <a.misrikhanova13@gmail.com>
2021-12-16 23:41:10 +0300

    display first job

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,33 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-  const [value,setValue] = useState(0)
+  const [value, setValue] = useState(0);
 
-  const fetchJobs = async() =>{
-    const response = await fetch(url)
-    const newJobs = await response.json()
-    setJobs(newJobs)
-    setLoading(false)
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
   }
-
-  useEffect(()=>{
-    fetchJobs()
-  },[])
-
-  
-
-  if(loading){
-return <section className='section loading'>
-  <h1>loading...</h1>
-</section>
-  }
-
-  return <h2>jobs</h2>
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/*btn container */}
+        {/*job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company} </h4>
+          <p className="job-dates">{dates} </p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty} </p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
