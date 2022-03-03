c374799a1985cf9e0a8cd049ad7339ed2859e48b
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-03-03 17:58:15 +0300

    connect to socket server

--- 08-realtime-colors/client/src/App.js
+++ 08-realtime-colors/client/src/App.js
@@ -1,8 +1,14 @@
 
 import './App.css';
+import { useEffect } from 'react';
+import { init } from './socketApi';
 import Palette from './components/Palette';
 
 function App() {
+
+  useEffect(()=>{
+    init()
+  },[])
   return (
     <div className="App">
       <Palette/>
