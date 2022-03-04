1ae0a7c4fdbaf5d0e415fe60e0a23223cb1aebdb
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-03-04 14:31:55 +0300

    preparation of the general view

---
+++ 09-todo-app-redux/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer class="info">
+      <p>Click to edit a todo</p>
+      <p>
+        Created by <a href="https://github.com/ayselcavdar">Aysel Cavdar</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
